5c44eb3504ed5edc442f0ac649acbce5f3fe3621
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-12 15:45:44 +0300

    from django.shortcuts import render, redirect

--- veiws.py
+++ veiws.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from .forms import SignUpForm
+from django.contrib.auth import login, authenticate
+
+
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
 
 
