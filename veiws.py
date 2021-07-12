26b3c0ca43d7d148306071960b898691062b355c
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-12 15:43:29 +0300

    Add more styling and edit various html templates

--- veiws.py
+++ veiws.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-
-
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -15,6 +13,6 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
-@login_required()
+@login_required(login_url='login')
 def index(request):
     return render(request, 'instagram/index.html')
