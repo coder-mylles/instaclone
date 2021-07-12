9e414183abdd56b5b8dc6f5f8780e305f244d02e
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-13 02:12:36 +0300

    Display all posts from the database

--- instagram/veiws.py
+++ instagram/veiws.py
@@ -3,6 +3,9 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm
 from django.contrib.auth import login, authenticate
+from .models import Post
+
+
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -16,9 +19,16 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
 @login_required(login_url='login')
 def index(request):
-    return render(request, 'instagram/index.html')
+    images = Post.objects.all()
+    params = {
+        'images': images,
+    }
+    return render(request, 'instagram/index.html', params)
+
+
 @login_required(login_url='login')
 def profile(request, username):
     if request.method == 'POST':
@@ -31,3 +41,8 @@ def profile(request, username):
     else:
         user_form = UpdateUserForm(instance=request.user)
         prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    params = {
+        'user_form': user_form,
+        'prof_form': prof_form,
+    }
+    return render(request, 'instagram/profile.html', params)
