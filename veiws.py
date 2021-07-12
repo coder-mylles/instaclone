7b15b9ef668308106aaf82e6de49bb806cc31622
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-12 19:25:27 +0300

    Add a Navbar for easiers navigation

--- veiws.py
+++ veiws.py
@@ -8,14 +8,17 @@ from django.contrib.auth import login, authenticate
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
             return redirect('index')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
-
-
 @login_required(login_url='login')
 def index(request):
     return render(request, 'instagram/index.html')
