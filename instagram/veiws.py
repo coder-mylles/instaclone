ca37c424ea6a786d25f0e994871f482baa64bc4d
martin-moringa <martin.mylles@student.moringachool.com>
2021-07-13 01:48:01 +0300

    update profile information and functionality of profile

--- instagram/veiws.py
+++ instagram/veiws.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, UpdateUserForm, UpdateUserProfileForm
 from django.contrib.auth import login, authenticate
-
-
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -21,16 +19,15 @@ def signup(request):
 @login_required(login_url='login')
 def index(request):
     return render(request, 'instagram/index.html')
-
-from django.urls import path, include
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('account/', include('django.contrib.auth.urls')),
-    path('', views.index, name='index'),
-    path('profile/<username>/', views.profile, name='profile'),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+@login_required(login_url='login')
+def profile(request, username):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
